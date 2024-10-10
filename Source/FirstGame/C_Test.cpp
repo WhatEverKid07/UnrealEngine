493a9764467b02fb511c5e379524ef8156186a4e
WhatEverKid07 <eugeniosanc22@outlook.com>
2024-10-10 12:46:25 +0100

    Finished Tutorial, added C++

---
+++ Source/FirstGame/C_Test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Test.h"
+
+C_Test::C_Test()
+{
+}
+
+C_Test::~C_Test()
+{
+}
---
+++ Source/FirstGame/C_Test.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FIRSTGAME_API C_Test
+{
+public:
+	C_Test();
+	~C_Test();
+};
